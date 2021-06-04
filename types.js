db231f70ac2c17ef02773b927053aaadc4aba11a
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-06-04 15:41:10 -0700

    add types

---
+++ types.js
@@ -0,0 +1,30 @@
+//console.log('Hello from typescript')
+// types
+var myString;
+var myNumber;
+var myBoolean;
+var myStrArr;
+var myNumArr;
+var myBooArr;
+var strNumBooTuple;
+var anything;
+var myUndif = undefined;
+var myNull = null;
+myString = "hello from mystring";
+myNumber = 1;
+myBoolean = false;
+myStrArr = ["str", "no numbers", "no booleans"];
+myNumArr = [2, 3, 4];
+myBooArr = [true, false, false];
+strNumBooTuple = ["str", 56, true];
+anything = ['stirng', 2];
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myStrArr);
+console.log(myNumArr);
+console.log(myBooArr);
+console.log(strNumBooTuple);
+console.log(anything);
+console.log(myUndif);
+console.log(myNull);
---
+++ types.ts
@@ -0,0 +1,46 @@
+//console.log('Hello from typescript')
+
+// types
+
+let myString: String;
+let myNumber: number;
+let myBoolean: boolean;
+
+let myStrArr: Array<string>;
+let myNumArr: Array<number>;
+let myBooArr: Array<boolean>;
+
+let strNumBooTuple: [string, number, boolean];
+
+let anything: any;
+
+let myUndif: void = undefined;
+let myNull: void = null;
+
+myString = "hello from mystring";
+myNumber = 1 
+myBoolean = false 
+
+myStrArr = ["str", "no numbers", "no booleans"]
+myNumArr = [2, 3, 4]
+myBooArr = [true, false, false]
+
+strNumBooTuple = ["str", 56, true]
+
+anything = ['stirng',2]
+
+
+console.log(myString)
+console.log(myNumber)
+console.log(myBoolean)
+
+console.log(myStrArr)
+console.log(myNumArr)
+console.log(myBooArr)
+
+console.log(strNumBooTuple)
+
+console.log(anything)
+
+console.log(myUndif)
+console.log(myNull)
